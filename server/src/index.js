077ea6e23cce53794eb330ae6b3a9d4d04f7480d
Arthur_Tu44 <88666036+ArthurTu44@users.noreply.github.com>
2024-03-08 23:38:16 -0300

    Arthur: Estrutura Base

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
+
+const api = express();
+
+api.use(cors());
+api.use(express.json()); 
+api.use('/', routes);
+
+api.listen(3333, () => {
+  console.log('Socando a API!!!')
+});
---
+++ server/src/repository/connectmysql.js
@@ -0,0 +1,15 @@
+import mysql2 from 'mysql2/promise';
+
+
+//mudar de acordo com o seu mysql workbench
+async function connect(){
+  return await mysql2.createConnection({
+    host:'localhost', //<-
+    port: 3306, //<-
+    password: '', //<-
+    database:'db_soc',
+    user:'root', //<-
+  })
+}
+
+export default {connect};
